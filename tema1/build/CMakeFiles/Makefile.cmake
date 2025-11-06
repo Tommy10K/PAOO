30cd84f37c559d9dbb71916bb58279d803003db6
Tommy10K <alexcykas3@gmail.com>
2025-11-06 17:01:30 +0200

    target run

--- tema1/build/CMakeFiles/Makefile.cmake
+++ tema1/build/CMakeFiles/Makefile.cmake
@@ -40,4 +40,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
   "CMakeFiles/program.dir/DependInfo.cmake"
+  "CMakeFiles/run.dir/DependInfo.cmake"
   )
